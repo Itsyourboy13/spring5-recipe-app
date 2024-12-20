cfeb4a8f8e788904851082da425e5b4fa42768d0
AnDrew Card <andrewbcard@gmail.com>
2024-12-20 17:17:20 +0000

    One to One UOM Relationships
    Added the class UnitOfMeasure which has a one to one relationship with the Ingredient class.
    Also added the uom variable to the Recipe class

--- src/main/java/guru/springframework/domain/UnitOfMeasure.java
+++ src/main/java/guru/springframework/domain/UnitOfMeasure.java
@@ -2,8 +2,10 @@ package guru.springframework.domain;
 
 import lombok.Data;
 
-import javax.persistence.*;
-import java.math.BigDecimal;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 /**
  * Created on 12/20/2024
@@ -11,14 +13,9 @@ import java.math.BigDecimal;
  */
 @Data
 @Entity
-public class Ingredient {
+public class UnitOfMeasure {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String description;
-    private BigDecimal amount;
-    @OneToOne(fetch = FetchType.EAGER)
-    private UnitOfMeasure uom;
-    @ManyToOne
-    private Recipe recipe;
 }
